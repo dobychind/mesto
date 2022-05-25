78524c1ffa44b000b1b5ba22d431be0a14ddc3f3
dobychind <dimadoby4in@yandex.ru>
2022-05-25 12:48:33 +0300

    3rd try ( problem with like)

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,31 @@ const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupExitButton = popup.querySelector('.popup__exit-button');
 const formElement = document.querySelector('.popup__form');
-const popupFieldName = popup.querySelector('.popup__field_name');
-const popupFieldDescription = popup.querySelector('.popup__field_description');
+const popupFieldName = popup.querySelector('.popup__field_type_name');
+const popupFieldDescription = popup.querySelector('.popup__field_type_description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+const likeButton = document.querySelector('.elements__like'); //работает только с 1 блоком \_(0_0)_/, если поменять elements__like на elements, то изображение будет меняться, но меняться будет весь блок.
 
-const popupToggle = function() {
+const popupToggle = function () {
   popup.classList.toggle('popup_opened');
-  profileName = profileName.value;
-  profileDescription = profileDescription.value;
+  popupFieldName.value = profileName.textContent;
+  popupFieldDescription.value = profileDescription.textContent;
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupFieldName.value;
   profileDescription.textContent = popupFieldDescription.value;
   popupToggle();
 }
 
+function likeChange(evt) {
+  console.log("click");
+  evt.target.classList.toggle('elements__like_active');
+};
+
+likeButton.addEventListener('click', likeChange);
 profileEditButton.addEventListener('click', popupToggle);
 popupExitButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler);
